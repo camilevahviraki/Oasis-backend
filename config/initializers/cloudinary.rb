f251ca2466f823f87d6b7ad399cdeb1dc458c108
camilevahviraki <camilevahviraki@gmail.com>
2023-01-10 21:46:12 +0200

    Add cloudinary config

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+    config.cloud_name = "dii6zzyqu"
+    config.api_key = "779862917371887"
+    config.api_secret = "1hj5UfGafzcUCvb3RR76hKgyG2k"
+    config.secure = true
+end

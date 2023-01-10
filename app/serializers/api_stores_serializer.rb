2805525d9ec8080f0c52baa64ef5ce86ec8b42e6
camilevahviraki <camilevahviraki@gmail.com>
2023-01-10 21:45:12 +0200

    Add serializers

---
+++ app/serializers/api_stores_serializer.rb
@@ -0,0 +1,22 @@
+class ApiStoresSerializer < ActiveModel::Serializer
+  
+    attributes :id, :name, :location, :country, :description, :user_id, :categories, :images_url,
+  
+    def categories
+       StoreCategory.where(store_id: object.id)
+    end    
+
+    def images_url
+      store_images = StoreImage.where(store_id: object.id)[0]
+      arr = []
+      if store_images
+        store_images.pictures.attachments.each do |picture|
+           arr << picture.url
+        end
+        return arr
+      else
+        images_url = []
+      end  
+    end
+  
+  end

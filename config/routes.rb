8c937c4ee91aa4140128336a78b5248513e4f7c3
camilevahviraki <camilevahviraki@gmail.com>
2023-01-20 23:28:31 +0200

    Update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
     ### User:
-    resources :users, only: %i[index show destroy update]
+    # resources :users, only: %i[index show destroy update]
+    
+    devise_for :users,
+    controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+    }
 
     # ##1: Stores
     get 'api_stores', to: 'api_stores#index'

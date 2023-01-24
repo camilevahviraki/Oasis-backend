f85a8915c1fc40100719dd6305da7a07e3e9f1c5
camilevahviraki <camilevahviraki@gmail.com>
2023-01-25 01:06:39 +0200

    Add jwt migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_223325) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_210428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -279,7 +279,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_223325) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "jti", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

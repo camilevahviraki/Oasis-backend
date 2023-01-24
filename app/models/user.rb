8883fe4ddfb7af6a9049aaed5aff69b2d48eca61
camilevahviraki <camilevahviraki@gmail.com>
2023-01-25 01:04:42 +0200

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
    
   has_many :stores, foreign_key: 'user_id', dependent: :destroy
   has_many :carts, foreign_key: 'user_id', dependent: :destroy
   has_many :orders, foreign_key: 'user_id', dependent: :destroy
+
 end

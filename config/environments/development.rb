e98e8348151fbbea38b7f6435c1dd971c0a917fe
camilevahviraki <camilevahviraki@gmail.com>
2023-01-30 14:24:52 +0200

    Update environment development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,6 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -39,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -69,4 +67,22 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # config.action_controller.forgery_protection_origin_check = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                  465,
+      domain:               'gmail.com',
+      user_name:            'camilux243@gmail.com',
+      password:             'ezoaeyvbwgrnkknf',
+      authentication:       'plain',
+      enable_starttls_auto: true,
+      ssl: true
+  }
+
 end

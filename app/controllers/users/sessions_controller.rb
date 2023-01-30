385bca6f3d6ef145d5ae7284a692eb81fb141d86
camilevahviraki <camilevahviraki@gmail.com>
2023-01-30 14:23:46 +0200

    Add sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,16 +1,37 @@
 class Users::SessionsController < Devise::SessionsController
+
     respond_to :json 
-    private 
+    private
+
     def respond_with(resource, _opts = {})
-      render json: { message: 'Logged.' }, status: :ok
+      if resource.persisted?
+        token = encode_token(user_id: resource.id)
+        render json: {
+          status: {code: 200, message: 'Logged in sucessfully.'},
+          user: UserSerializer.new(resource).serializable_hash[:data][:attributes],
+          token:
+        }, status: :ok
+
+      else
+        render json: {
+          status: 401,
+          message: "Couldn't find user.",
+        }, status: :ok
+      end  
     end
+  
     def respond_to_on_destroy
-      current_user ? log_out_success : log_out_failure
+      if current_user
+        render json: {
+          status: 200,
+          message: "logged out successfully"
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "Couldn't find an active session."
+        }, status: :ok
+      end
     end
-    def log_out_success
-      render json: { message: "Logged out." }, status: :ok
-    end
-    def log_out_failure
-      render json: { message: "Logged out failure."}, status: :unauthorized
-    end
+
   end

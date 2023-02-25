85cf8c1cdfe6af1e76545db44c59cabc0b7431fa
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 07:21:01 +0200

    Add item serializer and country serializer

--- app/serializers/api_stores_serializer.rb
+++ app/serializers/api_stores_serializer.rb
@@ -1,10 +1,10 @@
 class ApiStoresSerializer < ActiveModel::Serializer
   
-    attributes :id, :name, :location, :country, :description, :user_id, :categories, :images_url,
+    attributes :id, :name, :country_id, :location, :country, :description, :user_id, :categories, :images_url,
   
     def categories
        StoreCategory.where(store_id: object.id)
-    end    
+    end  
 
     def images_url
       store_images = StoreImage.where(store_id: object.id)[0]
---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,22 @@
+class ItemSerializer < ActiveModel::Serializer
+  
+    attributes :id, :names, :mainName, :description, :store_id, :currency, :quantity, :updated_at, :created_at, :price,
+    :items_images, :item_categories
+
+    def item_categories
+      ItemCategory.where(item_id: object.id)
+    end
+  
+    def items_images
+      images = ItemImage.where(item_id: object.id)[0]
+      arr = []
+      if images
+        images.pictures.attachments.each do |picture|
+          arr << picture.url
+        end
+        return arr
+      else
+        images_images = []
+      end  
+    end
+  end

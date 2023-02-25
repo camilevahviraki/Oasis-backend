65985f720a5b99c5a797fa52f93530cec10cf5de
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 07:17:41 +0200

    Add Item page Controller

---
+++ app/controllers/items/items_controller.rb
@@ -0,0 +1,68 @@
+class Items::ItemsController < ApplicationController
+    def index
+       category_id = params[:category_id]
+       category = params[:category]
+       store_id = params[:store_id]
+
+      #  data =  Item.left_outer_joins(:item_categories)
+      #     .select('items.*, item_categories.*, item_categories.id as category_id')
+      #     .where("items.store_id = #{store_id}")
+         #  .where("item_categories.name = #{category}")
+       data = Item.where(store_id: store_id)  
+
+       render :json => data, each_serializer: ItemSerializer
+    end
+ 
+    def show
+      item_id = params[:item_id]
+
+      item = Item.find(item_id)
+
+      render :json => item, serializer: ItemSerializer
+    end
+    
+    def create
+       store_id = params[:store_id]
+       description = params[:description]
+       pictures = params[:pictures]
+       mainName = params[:mainName]
+       names = params[:names]
+       price = params[:price]
+       quantity = params[:quantity]
+ 
+       @item = Item.new(
+          store_id: store_id,
+          description: description,
+          mainName: mainName,
+          names: names,
+          price: price,
+          quantity: quantity
+       )
+
+       category = ItemCategoriesList.where(name: params[:category])[0]
+ 
+       if @item.save
+         unless params[:category] == nil || params[:category] == 'all'
+            ItemCategory.create(
+              name: params[:category],
+              item_id: @item.id,
+              item_categories_list_id: category.id
+           )
+         end
+
+         @item_image = ItemImage.create(item_id: @item.id)
+
+         @item_image.pictures.attach(pictures)
+
+         if @item_image.pictures.attached?
+            render json: { message: 'created sucessfully', item_id: @item.id }
+         else
+            render json: {message: 'Error while attaching Images', item_id: @item.id }
+         end      
+       else
+         render json: { message: 'Failed to create store' }
+       end
+    end 
+ 
+ 
+ end    

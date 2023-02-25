f620b43c1d26b14644631fc3940c5995a81e509c
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 07:16:01 +0200

    Add Countries Controller

---
+++ app/controllers/countries/countries_controller.rb
@@ -0,0 +1,6 @@
+class Countries::CountriesController < ApplicationController
+    def index
+        countries = Country.all 
+        render :json => countries, each_serializer: CountriesSerializer
+    end 
+end

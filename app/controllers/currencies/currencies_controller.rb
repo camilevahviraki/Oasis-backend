aaece414e08ca53ef55e3c04995b0bd8fba91963
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 07:16:28 +0200

    Add Currencies Controller

---
+++ app/controllers/currencies/currencies_controller.rb
@@ -0,0 +1,11 @@
+class Currencies::CurrenciesController < ApplicationController
+    def index
+      currencies = Currency.all
+      render json: currencies
+    end
+    
+    def show
+      currency = Currency.find(params[:id])
+      render json: currency
+    end
+end

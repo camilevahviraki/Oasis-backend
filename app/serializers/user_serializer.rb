1f06ebaa44dfa4698326c97d6be0756970f4ac5b
camilevahviraki <camilevahviraki@gmail.com>
2023-07-13 01:25:29 +0200

    Update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :first_name, :last_name, :updated_at, :email, :avatar_url
+  attributes :id, :first_name, :last_name, :updated_at, :email, :avatar_url, :admin
 end

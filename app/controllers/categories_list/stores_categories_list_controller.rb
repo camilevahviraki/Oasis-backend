2d61124645faa7ca8dd09cac24fcccd2dfbb9464
camilevahviraki <camilevahviraki@gmail.com>
2023-07-12 23:55:42 +0200

    Update store_categories_list controller

--- app/controllers/categories_list/stores_categories_list_controller.rb
+++ app/controllers/categories_list/stores_categories_list_controller.rb
@@ -4,15 +4,15 @@ class CategoriesList::StoresCategoriesListController < ApplicationController
   end
 
   def create
-    params[:name]
-    params[:icon]
-    image = params[:icon]
+    name = params[:name]
+    icon = params[:icon]
+    image = params[:image]
 
-    category = StoreCategoriesList.new(:name)
+    category = StoreCategoriesList.new(name:)
 
     if category.save
       if image
-        category.attach(:image)
+        category.image.attach(:image)
         render json: { message: 'Store categoery type created successfully with image.' }
       else
         render json: { message: 'Store categoery type created successfully.' }
@@ -29,7 +29,7 @@ class CategoriesList::StoresCategoriesListController < ApplicationController
     category = StoreCategoriesList.find(id)
 
     if category
-      category.attach(:image)
+      category.image.attach(:image)
       render json: { message: 'Image attached successfully.' }
     else
       render json: { message: "Could not find Store Category with id: #{id}" }
